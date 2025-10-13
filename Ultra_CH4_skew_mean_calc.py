883551a11356c732b185d809d757099459fcf3e1
Max Lloyd <max.k.lloyd@gmail.com>
2025-10-12 22:13:37 -0400

    Added mean delta calculation for skewed data

---
+++ Ultra_CH4_skew_mean_calc.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 12 16:33:26 2025
+
+@author: Max
+"""
+
+import numpy as np
+import re
+import pandas as pd
+from scipy.stats import skewnorm, bootstrap
+# from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+# get sample path
+samplePath = '/Users/Max/Downloads/10-8_plus13CD_std/dD_MRAI/d_data_all.xlsx'
+
+# read in file
+d = pd.read_excel(samplePath, index_col=0)
+
+# 
+d['is_sample'] = d['is_sample'].astype(bool)
+cutoffVal = 60
+#assume the last one is the H4 peak
+RtoUse= re.findall('R1\d\_', d.columns[-2])[0] + 'stable'
+
+dg = d.groupby(['measure_line', 'is_sample'])
+dp = (dg['i17_raw'].count() - dg['is_off_peak'].sum())/dg['i17_raw'].count()
+d = d.merge((dp*100).reset_index(), how='left', on=['measure_line', 'is_sample'])
+d['percent_on_peak'] = d[0]
+d = d.drop(columns = [0])
+
+
+def statistic(data):
+    tFit = skewnorm.fit(data)
+    return(tFit[1])
+
+def bootstrap_skew_mean(data, num_resamples=1000):
+    """
+    Estimates the sampling distribution of the mean using bootstrap resampling.
+
+    Args:
+        data (array-like): The original dataset.
+        num_resamples (int): The number of bootstrap samples to generate.
+
+    Returns:
+        numpy.ndarray: An array of means from the bootstrap samples.
+    """
+    n = len(data)
+    bootstrap_means = []
+    for _ in range(num_resamples):
+        # Create a bootstrap sample by sampling with replacement
+        resample_indices = np.random.choice(n, n, replace=True)
+        bootstrap_sample = data[resample_indices]
+        bootstrap_means.append(statistic(bootstrap_sample))
+    return np.array(bootstrap_means)
+
+# fit sample and std, compute delta
+means = []
+fits = []
+boots = []
+fig, ax = plt.subplots(nrows=2)
+for b in [0, 1]:
+    theseData = d.loc[(d['is_sample']==bool(b)) & (d['percent_on_peak']>cutoffVal), RtoUse].values
+    thisH = ax[b].hist(theseData, bins=50, density=True)
+    xlim = ax[b].get_xlim()
+    
+    xRange = np.linspace(*xlim, num=1000)
+    thisFit = skewnorm.fit(theseData)
+    fits.append(thisFit)
+    means.append(thisFit[1])
+    ax[b].plot(xRange, skewnorm.pdf(xRange, *thisFit), '-')
+    ax[b].set_xlim(*xlim)
+    
+    
+    boots.append(bootstrap_skew_mean(theseData, num_resamples=1000))
+    
+    
+
+booty = np.stack(boots)
+meany = np.asarray(means)
+delta = (meany[1]/meany[0]-1)*1000
+deltaSE = np.sqrt(((booty.std(axis=1)/meany)**2).sum())*(meany[1]/meany[0])*1000
+result = "delta: {0:.3f} ± {1:.3f}‰".format(delta,deltaSE)
+
+ax[b].text(0.05, 0.95, result, transform=ax[b].transAxes, va='top')
+fig.savefig('skewFits.pdf')
+
+print(result)
+
+with open("skewDelta.txt", "w") as file:
+    file.write(result)
+
+
+
+
+
+
+
+
+
